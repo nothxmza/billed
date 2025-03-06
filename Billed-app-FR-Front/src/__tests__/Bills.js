4ee2700c5c07493b1886b70f758950a409bb270c
nothxmza <terras.hamza@hotmail.fr>
2025-03-06 16:56:18 +0100

    billed bugs and test 1 2 3 4 finish

---
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -0,0 +1,57 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {screen, waitFor, fireEvent, getByTestId} from "@testing-library/dom"
+import BillsUI from "../views/BillsUI.js"
+import { bills } from "../fixtures/bills.js"
+import { ROUTES_PATH} from "../constants/routes.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+
+import router from "../app/Router.js";
+import e from "express";
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('icon-window'))
+      const windowIcon = screen.getByTestId('icon-window')
+      //to-do write expect expression
+      expect(windowIcon).toBeTruthy()
+
+    })
+    test("Then bills should be ordered from earliest to latest", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted)
+    })
+    test("then eye icon should open modal", async () => {
+      const eye = screen.getAllByTestId('icon-eye')
+      expect(eye).toBeTruthy()
+      fireEvent.click(eye[1])
+      await waitFor(() => screen.getByTestId("modaleFile"))
+      const modale = screen.getByTestId("modaleFile")
+      expect(modale).toBeTruthy()
+    })
+    test("Then clicking on new bill button should navigate to new bill page", async () => {
+      const buttonNewBill = screen.getByTestId('btn-new-bill')
+      fireEvent.click(buttonNewBill)
+      await waitFor(() => screen.getByTestId('form-new-bill'))
+      const formNewBill = screen.getByTestId('form-new-bill')
+      expect(formNewBill).toBeTruthy()
+    })
+
+  })
+})
